6c78618b02e9aea0d213086fb62f41b71a0fc1ea
Kathryn Killebrew <kkillebrew@azavea.com>
2017-02-08 17:02:05 -0500

    Create tables with bounded network data
    For stress calculations, create intermediate tables of road network data filtered to bounds,
    rather than filtering within queries. Results in ~10x speedup.

--- connectivity/reachable_roads_low_stress_calc.sql
+++ connectivity/reachable_roads_low_stress_calc.sql
@@ -11,26 +11,18 @@ INSERT INTO generated.neighborhood_reachable_roads_low_stress (
 SELECT  r1.road_id,
         v2.road_id,
         sheds.agg_cost
-FROM    neighborhood_ways r1
+FROM    neighborhood_ways_bounded r1
         INNER JOIN neighborhood_ways_net_vert v1 ON (r1.road_id = v1.road_id),
         pgr_drivingDistance('
-            SELECT     nl.link_id AS id,
-                       nl.source_vert AS source,
-                       nl.target_vert AS target,
-                       nl.link_cost AS cost
-            FROM       neighborhood_ways_net_link AS nl
-            INNER JOIN neighborhood_boundary AS nb
-            ON         ST_Intersects(nl.geom, nb.geom)
-            WHERE      link_stress = 1',
+            SELECT  link_id AS id,
+                    source_vert AS source,
+                    target_vert AS target,
+                    link_cost AS cost
+            FROM    neighborhood_ways_net_link_bounded
+            WHERE   link_stress = 1',
             v1.vert_id,
             10560,
             directed := true
         ) sheds
         INNER JOIN neighborhood_ways_net_vert v2 ON (v2.vert_id = sheds.node)
-WHERE r1.road_id % :thread_num = :thread_no
-AND
-EXISTS (
-            SELECT  1
-            FROM    neighborhood_boundary AS b
-            WHERE   ST_Intersects(b.geom, r1.geom)
-);
+WHERE r1.road_id % :thread_num = :thread_no;
